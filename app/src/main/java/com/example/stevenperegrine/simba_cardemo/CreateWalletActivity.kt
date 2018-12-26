c81264ded2815a7218985c8d5dd64cbe743f5ed4
Speregrine <38218791+Speregrine@users.noreply.github.com>
2018-12-26 17:02:43 -0500

    -can import and take photo for imageview in post screen -can now generate wallet address off of a predefined seed

--- app/src/main/java/com/example/stevenperegrine/simba_cardemo/CreateWalletActivity.kt
+++ app/src/main/java/com/example/stevenperegrine/simba_cardemo/CreateWalletActivity.kt
@@ -3,8 +3,9 @@ package com.example.stevenperegrine.simba_cardemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
-import io.github.novacrypto.bip39.SeedCalculator
+
 import kotlinx.android.synthetic.main.activity_createwallet.*
 import org.web3j.abi.datatypes.Address
 import org.web3j.crypto.*
@@ -12,7 +13,11 @@ import org.web3j.protocol.Web3j
 import java.security.KeyPair
 import java.util.UUID.randomUUID
 import java.util.UUID
-
+import org.bitcoinj.crypto.HDUtils
+import org.bitcoinj.crypto.ChildNumber
+import org.bitcoinj.wallet.DeterministicKeyChain
+import org.bitcoinj.wallet.DeterministicSeed
+import org.bitcoinj.wallet.KeyChain
 
 
 class CreateWalletActivity : AppCompatActivity() {
@@ -25,6 +30,24 @@ class CreateWalletActivity : AppCompatActivity() {
 
 
 
+    }
+    fun createWallet(view:View)
+    {
+        val seedCode = "jeans absorb curve mimic task apology green ability cake eyebrow report inner"
+
+// BitcoinJ
+        val seed = DeterministicSeed(seedCode, null, "", 1409478661L)
+        val chain = DeterministicKeyChain.builder().seed(seed).build()
+
+
+        val keyPath = HDUtils.parsePath("M/44H/60H/0H/0/0")
+        val key = chain.getKeyByPath(keyPath, true)
+        val privKey = key.privKey
+
+// Web3j
+        val credentials = Credentials.create(privKey.toString(16))
+        createAddress.text = credentials.address
+        createPrivateKey.text = privKey.toString()
     }
 
 }
--- app/src/main/java/com/example/stevenperegrine/simba_cardemo/PostActivity.kt
+++ app/src/main/java/com/example/stevenperegrine/simba_cardemo/PostActivity.kt
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.content.Intent
 import android.provider.MediaStore
+import android.graphics.Bitmap
+import kotlinx.android.synthetic.main.activity_post.*
+
 
 
 
@@ -19,6 +22,7 @@ class PostActivity : AppCompatActivity() {
 
 
     }
+    //Take Car Image
     fun TakeCarImage(view:View){
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(packageManager)?.also {
@@ -27,10 +31,41 @@ class PostActivity : AppCompatActivity() {
         }
 
     }
- /*   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data.extras.get("data") as Bitmap
-            mImageView.setImageBitmap(imageBitmap)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            postImage.setImageBitmap(imageBitmap)
+           // postImage.rotation = 0F
+        }
+        if (requestCode === 2 && resultCode === RESULT_OK) {
+            val imageUri = data?.getData()
+            postImage.setImageURI(imageUri)
+          //  postImage.rotation = 90F
+
+        }
+    }
+    //Load Car Image
+    fun pickImage(view:View) {
+
+        val gallery = Intent(Intent.ACTION_GET_CONTENT)
+        gallery.type = "image/*"
+        startActivityForResult(gallery, 2)
+
+    }
+
+  /*  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 1) {
+
+                //Get ImageURi and load with help of picasso
+                //Uri selectedImageURI = data.getData();
+
+                Picasso.with(this@PostActivity).load(data!!.data).noPlaceholder().centerCrop().fit()
+                    .into(findViewById(R.id.postImage) as ImageView)
+            }
+
         }
     }*/
+
 }
